16da0dde1de3441d26cf3089e74d72640feb9ca0
Noah Paige <noahlandonpaige@gmail.com>
2022-02-19 12:39:55 -0800

    added material UI

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import Button from '@mui/material/Button';
+
 function App() {
   return (
     <div className="App">
@@ -18,7 +25,9 @@ function App() {
         >
           Learn React
         </a>
+        <Button variant="contained"> Hello Wurld </Button>
       </header>
+      
     </div>
   );
 }
